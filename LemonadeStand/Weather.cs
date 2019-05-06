a92b9b94f36d7697df22b4aa0b21ad024002941f
Robert Smidt <rsmidt909@hotmail.com>
2019-05-06 11:56:08 -0500

    Added in weather chooser switch case for both the current day and the next day.

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -14,21 +14,83 @@ namespace LemonadeStand
         //member variables (HAS A)
         string currentWeather;
         string forecastedWeather;
+        
 
 
 
 
         //Constructor (BUILDS OBJECT)
-
+        public Weather()
+        {
+            currentWeather = null;
+            forecastedWeather = null;
+        }
 
 
 
 
         //member methods(CAN DO)
+        public void WeatherChooserToday()
+        {
+            int weatherDecision = RandomNumber(1, 4);
+            switch (weatherDecision)
+            {
+                case 1:
+                    currentWeather = "Hot";
+                    break;
+                case 2:
+                    currentWeather = "Cold";
+                    break;
+                case 3:
+                    currentWeather = "Rainy";
+                    break;
+                case 4:
+                    currentWeather = "Nice";
+                    break;
+                default:
+                    currentWeather = "Something broke the weather chooser.";
+                     break;
+            }
+        }
+
+
+        public void WeatherChooserTomorrow()
+        {
+            int weatherDecision = RandomNumber(1, 4);
+            switch (weatherDecision)
+            {
+                case 1:
+                    forecastedWeather = "Hot";
+                    break;
+                case 2:
+                    forecastedWeather = "Cold";
+                    break;
+                case 3:
+                    forecastedWeather = "Rainy";
+                    break;
+                case 4:
+                    forecastedWeather = "Nice";
+                    break;
+                default:
+                    forecastedWeather = "Something broke the weather chooser.";
+                    break;
+            }
+        }
+
+
+        public int RandomNumber(int min, int max)
+        {
+            Random rando = new Random();
+            return rando.Next(min, max);
+        }
 
 
 
 
 
-    }
+
+
+    }   
 }
+
+
